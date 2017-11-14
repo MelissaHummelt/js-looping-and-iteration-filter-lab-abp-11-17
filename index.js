344409d3c3858c2985398de11237c8436cbfe5fd
Melissa Hummelt <mkhummelt@gmail.com>
2017-11-14 21:31:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
-function findMatching (list, name) {
-  return list.filter(function (driverName) {
+function findMatching (drivers, name) {
+  return drivers.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase();
   });
 }
