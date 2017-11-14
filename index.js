b0ab250cf135a082acd1fcfde12587d0d836d0de
Melissa Hummelt <mkhummelt@gmail.com>
2017-11-14 21:31:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Code your solution in this file
 function findMatching (drivers, name) {
-  let matchingDrivers = drivers.filter(function (driverName) {
-    if (name.toLowerCase() === driverName.toLowerCase()){
-      return true
-    } else {
-      return false
-    }
-  })
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
